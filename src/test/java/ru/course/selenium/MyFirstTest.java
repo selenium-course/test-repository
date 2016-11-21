2b9bc49ee915ac07c6055e358f30f28f150e88f2
Alex <mail-for-spam-only@rambler.ru>
2016-11-21 22:11:39 +0300

    First task of Selenium Course

---
+++ src/test/java/ru/course/selenium/MyFirstTest.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Aleksei.Klimenko on 21.11.2016.
+ */
+package ru.course.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeClass
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.navigate().to("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @AfterClass
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
